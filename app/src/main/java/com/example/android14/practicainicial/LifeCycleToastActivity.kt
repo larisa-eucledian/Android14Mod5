b63eafb9bb6cbe7055d65d6773c49cbfc1aef906
Larisa Clemenceau <larisa@eucledian.com>
2025-08-25 11:53:10 +0200

    Práctica Inicial: LifeCycle Toast & Implicit Send URL Intent

--- app/src/main/java/com/example/android14/practicainicial/LifeCycleToastActivity.kt
+++ app/src/main/java/com/example/android14/practicainicial/LifeCycleToastActivity.kt
@@ -1,54 +1,59 @@
-package com.example.android14.practicas
+package com.example.android14.practicainicial
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.android14.R
 
-class LifeCycleActivity : AppCompatActivity() {
+class LifeCycleToastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_life_cycle)
+        setContentView(R.layout.activity_life_cycle_toast)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Log.e("LifeCycleActivity", "onCreate")
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+
+    val btnSendUrl = findViewById<Button>(R.id.btnSendUrl)
+
+        btnSendUrl.setOnClickListener {
+            val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"))
+            startActivity(Intent.createChooser(urlIntent,"Open URL with:"))
+        }
 
     }
 
     override fun onStart() {
         super.onStart()
-        Log.e("LifeCycleActivity", "onStart")
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
-        Log.e("LifeCycleActivity", "onResume")
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
         super.onPause()
-        Log.e("LifeCycleActivity", "onPause")
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
-        Log.e("LifeCycleActivity", "onStop")
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e("LifeCycleActivity", "onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.e("LifeCycleActivity", "onRestart")
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
